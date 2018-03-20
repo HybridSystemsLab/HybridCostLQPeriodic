e48eab82c3ca052c7496039e498977b905c89653
Francesco Ferrante <francesco.ferrante@gipsa-lab.fr>
2018-03-21 00:47:20 +0100

    periodic jumps

--- Riccati.m
+++ Riccati.m
@@ -1,11 +1,16 @@
+%This script provide the solution to the Lyapunov equaiton in Example 1 
+
 clear all;
+
 Ac=[1 1;
     0 0];
 
 Ad=[1 0; -2 0];
 
 Qc=eye(2);
+
 Qd=eye(2);
+
 T=1;
 
 global Ac Ad Qd Qc T;
@@ -18,6 +23,7 @@ H=[Ac, zeros(n,n);
 Psi_0=expm(-H*T);
 
 psi_0_2_1=Psi_0(n+1:2*n,1:n);
+
 %Solve the Lyapunov DT equation
 %X-Ad'*expm(Ac'*T)*X*expm(Ac'*T)*Ad=Ad'*psi_0(2,1)*expm(Ac*T)*Ad+Qd
 
@@ -29,10 +35,5 @@ X=dlyap(Alyap,Qlyap);
 
 P_0=(psi_0_2_1+expm(Ac'*T)*X)*expm(Ac*T);
 
-syms tau
 
-Psi_t=expm(H*(tau-T));
-psi_t_2_1=Psi_t(n+1:2*n,1:n);
-
-P_t=(psi_t_2_1+expm(-Ac'*(tau-T))*X)*expm(-Ac*(tau-T));
 
